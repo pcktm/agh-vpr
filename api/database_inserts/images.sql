7e5b80ee06bfe811a5435b5b43f66d372684ce1a
aneta <anetapostrozny@gmail.com>
2022-12-03 21:10:31 +0100

    test database inserts

---
+++ api/database_inserts/images.sql
@@ -0,0 +1,4 @@
+INSERT INTO images (id, place_id, image) VALUES (1, 1, 'images/strazacka1.jpg');
+INSERT INTO images (id, place_id, image) VALUES (2, 1, 'images/strazacka2.JPG');
+INSERT INTO images (id, place_id, image) VALUES (3, 2, 'images/rozana.JPG');
+INSERT INTO images (id, place_id, image) VALUES (4, 3, 'images/florianska1.jpg');
---
+++ api/database_inserts/places.sql
@@ -0,0 +1,3 @@
+INSERT INTO places (id, name, address, description) VALUES (1, 'Test ', 'Kraków ', 'Miejsce testowe ');
+INSERT INTO places (id, name, address, description) VALUES (2, 'Różana', 'Kraków nwm', 'test kolejny no ');
+INSERT INTO places (id, name, address, description) VALUES (3, 'Floriańska', 'Karków idk', 'test ciąg dalszy ');
